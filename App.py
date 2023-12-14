f7a1a746af76df2d4f90339567f82e3f6d44d2b0
Bergam0t <s.j.rosser@exeter.ac.uk>
2023-12-14 14:14:10 +0000

    Improve labels; add ability to see individual log

--- App.py
+++ App.py
@@ -78,16 +78,6 @@ if uploaded_file is not None:
 
     concurrent_attendees_count_df = pd.DataFrame.from_dict(interval_census_list)
 
-    attendee_history_fig = px.line(concurrent_attendees_count_df, 
-                                x="time", y="attendee_count")
-
-    attendee_history_fig.update_yaxes(
-        tick0=0, dtick=10,
-        range=[0, concurrent_attendees_count_df['attendee_count'].max()+10]
-        )
-
-    # attendee_history_fig.show()
-
     maximum_simultaneous_attendees = concurrent_attendees_count_df['attendee_count'].max()
 
     col1, col2, col3 = st.columns(3)
@@ -131,19 +121,46 @@ if uploaded_file is not None:
             median_duration = f"{median_duration} minutes"
 
         st.metric(label="Median Duration of Attendance",
-                  value = f"{median_duration} minutes")
+                  value = median_duration)
 
     with col6:
+        total_duration = sum(guests_duration['TotalDuration'])
+        if total_duration > 60:
+            hours = int(total_duration // 60)
+            minutes = int(np.floor(total_duration % 60))
+            total_duration = f"{hours} hours {minutes} minutes"
+        else:
+            total_duration = f"{total_duration} minutes"
         st.metric(label="Total Duration of Attendance",
-                value = f"{sum(guests_duration['TotalDuration'])} minutes")
+                value = total_duration)
+
+
+    attendee_history_fig = px.line(concurrent_attendees_count_df, 
+                                x="time", y="attendee_count",
+                                title=f"Concurrent Attendees Over Time: {uploaded_file.name}")
+
+    attendee_history_fig.update_yaxes(
+        tick0=0, dtick=10,
+        range=[0, concurrent_attendees_count_df['attendee_count'].max()+10]
+        )
+
+    attendee_history_fig.update_layout(yaxis_title="Attendees present", 
+                                   xaxis_title="Time")
 
     st.plotly_chart(attendee_history_fig,
                     use_container_width=True)
 
     # st.select("Search for an attendee", )
 
+
+    duration_distribution_fig = px.histogram(guests_duration, x="TotalDuration",
+                                title=f"Distribution of Total Attendance Time: {uploaded_file.name}")
+
+    duration_distribution_fig.update_layout(yaxis_title="Number of Attendees in Time Category", 
+                                   xaxis_title="Total Time Spent in Meeting")
+
     st.plotly_chart(
-        px.histogram(guests_duration, x="TotalDuration"),
+        duration_distribution_fig,
         use_container_width=True
         )
 
@@ -152,3 +169,47 @@ if uploaded_file is not None:
 
     with st.expander("Click here to view the full log"):
         AgGrid(attendance_log)
+
+
+    # View attendance breakdown for a single person
+    selected_attendee = st.selectbox(label="Select an Attendee",
+                 options=guests
+              )
+    
+    filtered_attendance_log = attendance_log[attendance_log["Name (Original Name)"] == selected_attendee]
+
+    if len(filtered_attendance_log) > 0:
+        filtered_attendance_log["Join Time"] = pd.to_datetime(filtered_attendance_log["Join Time"])
+        filtered_attendance_log["Leave Time"] = pd.to_datetime(filtered_attendance_log["Leave Time"])
+
+        filtered_interval_census_list = []
+
+        i = pd.date_range(start.replace(second=0, microsecond=0),
+                end.replace(second=0, microsecond=0) + timedelta(minutes=1),
+                freq='1min')
+
+        for minute in i:
+            filtered_interval_census_list.append(
+                {"time": minute,
+                "attendee_count": len(filtered_attendance_log[(filtered_attendance_log["Join Time"] <= minute) &
+                                                    (filtered_attendance_log["Leave Time"] >= minute)]
+                                                    )})
+
+        filtered_concurrent_attendees_count_df = pd.DataFrame.from_dict(filtered_interval_census_list)    
+        
+        individual_attendee_history_fig = px.line(filtered_concurrent_attendees_count_df, 
+                                    x="time", y="attendee_count",
+                                    title=f"Zoom Attendance Log: {uploaded_file.name}")
+
+        individual_attendee_history_fig.update_yaxes(
+            tick0=0, dtick=10,
+            range=[0, filtered_concurrent_attendees_count_df['attendee_count'].max()+1]
+            )
+
+        individual_attendee_history_fig.update_layout(yaxis_title="Attendee present", 
+                                    xaxis_title="Time")
+
+        st.plotly_chart(individual_attendee_history_fig,
+                        use_container_width=True)
+    else:
+        st.markdown("No data for selected individual")
